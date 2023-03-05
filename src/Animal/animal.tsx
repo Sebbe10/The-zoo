c5dc77f239602bdd84a341af620e31868c0e30c6
Sebastian Bjelkstam <sebastian.bjelkstam@gmail.com>
2023-03-05 10:18:59 +0100

    React api

--- src/Animal/animal.tsx
+++ src/Animal/animal.tsx
@@ -15,7 +15,7 @@ function Animal() {
   useEffect(() => {
     async function oneAnimal() {
       let response = await axios.get(
-        "https://animals.azurewebsites.net/api/animals/" + i
+        "https://animals.azurewebsites.net/api/animals/" + id
       );
 
       setAnimal(response.data);
